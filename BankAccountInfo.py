5c315aeadfcb9db96d0ce5a0347b225f03c40683
ekinkade21 <42971395+ekinkade21@users.noreply.github.com>
2018-10-16 10:22:11 -0400

    Add files via upload

---
+++ BankAccountInfo.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 16, 2018
+
+@author: ekinkade20
+'''
+firstName=input("Please enter the first account holder's name:")
+firstAccountNum=int(input("Please enter the first account number:"))
+firstType=input("Please enter the type of account:")
+firstBalance=float(input("Please enter the balance:"))
+secondName=input("Please enter the second account holder's name:")
+secondAccountNum=int(input("Please enter the second account number:"))
+secondType=input("Please enter the type of account:")
+secondBalance=float(input("Please enter the balance:"))
+thirdName=input("Please enter the third account holder's name:")
+thirdAccountNum=int(input("Please enter the third account number:"))
+thirdType=input("Please enter the type of account:")
+thirdBalance=float(input("Please enter the balance:"))
+print()
+print("All Accounts:")
+print("Account Num:",format(firstAccountNum,'>011d'),format("Name:",'>8s'),format(firstAccountName,'>20s'),format())
